46db01acb1118d5bf9fc4b2b24f253f225ec4e75
Iker Lerones <iker.lerones@ikasle.egibide.org>
2022-05-10 20:37:21 +0200

    Funciones VM
    Falta la de visualizar los datos

--- Proyecto_Android_Final_Iker_Markel/app/src/main/java/com/example/proyecto_android_final_iker_markel/VM.kt
+++ Proyecto_Android_Final_Iker_Markel/app/src/main/java/com/example/proyecto_android_final_iker_markel/VM.kt
@@ -1,5 +1,66 @@
 package com.example.proyecto_android_final_iker_markel
 
-class VM {
+import android.widget.Toast
+import androidx.lifecycle.ViewModel
+import com.parse.ParseObject
+import com.parse.ParseQuery
+import kotlinx.coroutines.currentCoroutineContext
 
+class VM : ViewModel() {
+
+    private fun insertarLibro(titulo: String, autor: String, genero: String, fecha: Int) {
+
+        val objeto = ParseObject("Libros")
+        objeto.put("Titulo", titulo)
+        if (autor != null) {
+            objeto.put("Autor", autor)
+        }
+        objeto.put("Genero", genero)
+        objeto.put("Fecha", fecha)
+        objeto.saveInBackground {
+
+        }
+    }
+
+    private fun eliminarLibro(id: String) {
+
+        val query = ParseQuery.getQuery<ParseObject>("Libros")
+        query.whereEqualTo("objectId", id)
+        query.getFirstInBackground { parseObject, parseException ->
+            if (parseException == null) {
+                parseObject.deleteInBackground {
+                }
+            }
+        }
+    }
+
+    private fun modificarLibro(
+        id: String,
+        titulo: String,
+        autor: String,
+        genero: String,
+        fecha: Int
+    ) {
+
+        val query = ParseQuery.getQuery<ParseObject>("Libros")
+        query.whereEqualTo("objectId", id)
+        query.getFirstInBackground { parseObject, parseException ->
+            if (parseException == null) {
+                if (titulo != null) {
+                    parseObject.put("Titulo", titulo)
+                }
+                if (autor != null) {
+                    parseObject.put("Autor", autor)
+                }
+                if (genero != null) {
+                    parseObject.put("Genero", genero)
+                }
+                if (fecha != null) {
+                    parseObject.put("Fecha", fecha)
+                }
+                parseObject.saveInBackground {
+                }
+            }
+        }
+    }
 }
