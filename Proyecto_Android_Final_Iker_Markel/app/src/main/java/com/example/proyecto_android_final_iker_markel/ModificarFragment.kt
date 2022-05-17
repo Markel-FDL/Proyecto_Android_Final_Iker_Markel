2d7ab9789b63654d0ad5c5cad49d0760cb5cfa4a
Markel Fernandez de Labastida <markel.fernandezdelabastida@ikasle.egibide.org>
2022-05-17 21:02:50 +0200

    Modificaciones modificar libro

--- Proyecto_Android_Final_Iker_Markel/app/src/main/java/com/example/proyecto_android_final_iker_markel/ModificarFragment.kt
+++ Proyecto_Android_Final_Iker_Markel/app/src/main/java/com/example/proyecto_android_final_iker_markel/ModificarFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.example.proyecto_android_final_iker_markel.databinding.FragmentModificarBinding
 
@@ -27,11 +28,21 @@ class ModificarFragment : Fragment() {
 
         val id:Int=arguments?.getInt("id") ?:-1
 
-        binding.InserTitulo.
-        binding.InserAutor.
-        binding.InserGenero.
-        binding.InserFecha.
-        binding.Inser.isVisible=true
+        if(binding.ModTitulo.text.isNotEmpty() &&
+            binding.ModAutor.text.isNotEmpty() &&
+            binding.ModGenero.text.isNotEmpty() &&
+            binding.ModFecha.text.isNotEmpty()) {
+            if (binding.ModTitulo.text.toString() != (activity as MainActivity).miViewModel.listaLibros[id].titulo ||
+                binding.ModAutor.text.toString() != (activity as MainActivity).miViewModel.listaLibros[id].autor ||
+                binding.ModGenero.text.toString() != (activity as MainActivity).miViewModel.listaLibros[id].genero ||
+                binding.ModFecha.text.toString().toInt() != (activity as MainActivity).miViewModel.listaLibros[id].fecha) {
+                (activity as MainActivity).miViewModel.modificarLibro(id.toString(), binding.ModTitulo.text.toString(),
+                    binding.ModAutor.text.toString(), binding.ModGenero.text.toString(),  binding.ModFecha.text.toString().toInt())
+
+                Toast.makeText(activity,"Libro editado", Toast.LENGTH_LONG).show()
+                findNavController().navigate(R.id.action_modificarFragment_to_FirstFragment)
+            }
+        }
 
         binding.ModModificar.setOnClickListener {
             findNavController().navigate(R.id.action_mostrarFragment3_to_FirstFragment)
